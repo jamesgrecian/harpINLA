fb6fba0128b28be5b747e5191288d0b44feb45aa
James Grecian <james.grecian@gmail.com>
2021-02-02 15:45:00 +0000

    vectorise f.ice

--- R/f.ice.R
+++ R/f.ice.R
@@ -3,40 +3,38 @@
 # function input is season (1:4) and year (1:5) from which time point t is calculated (1:20)
 
 f.ice <- function(x, y, season, year) {
-  # turn coordinates into SpatialPoints object:
-  # with the appropriate coordinate reference system (CRS)
-  spp <- SpatialPoints(data.frame(x = x, y = y), proj4string = fm_sp_get_crs(ice))
-  proj4string(spp) <- fm_sp_get_crs(ice)
+
+  # convert season and year to indices for raster stack
+  t <- (year - 1) * 4 + season
   
-  # convert season and year to indices for raster stack 
-  if (year == 1){
-    t <- season
-  } else
-  if (year == 2){
-    t <- 4 + season
-  } else
-    if (year == 3){
-      t <- 8 + season
-    } else
-  if (year == 4){
-    t <- 12 + season
-  } else
-  if (year == 5){
-    t <- 16 + season
+  # extract space time matched sea ice to points
+  # loop through unique layers
+  v <- rep(NA, length(t))
+
+  for(tt in unique(t)){
+    
+    # turn coordinates into SpatialPoints object:
+    # with the appropriate coordinate reference system (CRS)
+    # and the appropriate time indices
+    spp <- SpatialPoints(coords = cbind(x = x[t == tt], y = y[t == tt]), proj4string = fm_sp_get_crs(ice))
+    proj4string(spp) <- fm_sp_get_crs(ice)
+
+    # Subset raster stack at correct time index
+    ice_sub <- raster::subset(ice, tt)
+    # Convert to SpatialPixelsDataFrame for inlabru extract
+    ice_sub <- as(ice_sub, "SpatialPixelsDataFrame")
+    
+    # Extract season and year specific sea ice values at spp coords
+    result <- over(spp, ice_sub)
+    names(result) <- "ice"
+    
+    if (any(is.na(result$ice))) {
+      result$ice <- inlabru:::bru_fill_missing(ice_sub, spp, result$ice)
+    }
+    
+    v[t == tt] <- result$ice
   }
-  
-  # Subset raster stack at correct time index
-  ice_sub <- raster::subset(ice, t)
-  # Convert to SpatialPixelsDataFrame for inlabru extract
-  ice_sub <- as(ice_sub, "SpatialPixelsDataFrame")
-  
-  # Extract season and year specific sea ice values at spp coords
-  v <- over(spp, ice_sub)
-  names(v) <- "ice"
-  if (any(is.na(v$ice))) {
-    v$ice <- inlabru:::bru_fill_missing(ice_sub, spp, v$ice)
-  }
-  return(v$ice)
+  return(v)
 }
 
 # ends
